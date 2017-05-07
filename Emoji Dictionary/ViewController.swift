fb6cc83ff14dfc4a602b57e06e2f083cc6bdaf7c
My first commit <apple@mac.local>
2017-05-07 17:56:29 +0800

    Our real first commit. Stupid Xcode

---
+++ Emoji Dictionary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Apple on 2017/5/7.
+//  Copyright © 2017年 Zappy Code. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
